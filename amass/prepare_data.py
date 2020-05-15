a9980bc5d246596082e2e3a0351b0ba7a93f4ba3
Do <mcdooooo@gmail.com>
2020-05-15 15:30:39 +0800

    Update prepare_data.py

--- amass/prepare_data.py
+++ amass/prepare_data.py
@@ -149,27 +149,23 @@ def dump_amass2pytroch(datasets, amass_dir, out_posepath, logger=None, betas_ran
                         assert betas_range % 2 == 0, ValueError('betas_range should be multiple to 2')
                         # if `betas_range` is an integer, 
                         # sample the number of betas1 and betas2 
-                        # that varience from -2. to 2. and betas3 in -5. to 5. as follows:
-                        beta1, beta2, beta3 = cdata['betas'][0], cdata['betas'][1], cdata['betas'][3]
+                        # that varience from -2. to 2. as follows:
+                        beta1, beta2 = cdata['betas'][0], cdata['betas'][1]
                         # left range, right range
                         beta1_lr, beta1_rr = max(0., 2. + beta1), max(0., 2. - beta1)
                         beta2_lr, beta2_rr = max(0., 2. + beta2), max(0., 2. - beta2)
-                        beta3_lr, beta3_rr = max(0., 5. + beta3), max(0., 5. - beta3)
                         # left range percentage, right range percentage
                         beta1_lp, beta1_rp = beta1_lr / (beta1_lr + beta1_rr),  beta1_rr / (beta1_lr + beta1_rr)
                         beta2_lp, beta2_rp = beta2_lr / (beta2_lr + beta2_rr),  beta2_rr / (beta2_lr + beta2_rr)
-                        beta3_lp, beta3_rp = beta3_lr / (beta3_lr + beta3_rr),  beta3_rr / (beta3_lr + beta3_rr)
                         # left range sample number
-                        beta1_ln, beta2_ln, beta3_ln = int(betas_range * beta1_lp), int(betas_range * beta2_lp), int(betas_range * beta3_lp)
+                        beta1_ln, beta2_ln = int(betas_range * beta1_lp), int(betas_range * beta2_lp)
                         # do sampling for beta1 range
                         beta1_range = betas_range_sample(betas_range, beta1, beta1_ln, 2.)
                         beta2_range = betas_range_sample(betas_range, beta2, beta2_ln, 2.)
-                        beta3_range = betas_range_sample(betas_range, beta3, beta3_ln, 5.)
                         # reconstruct beatas_range as numpy.ndarray
                         betas_range = np.zeros((betas_range, len(cdata['betas'])))
                         betas_range[:, 0] = beta1_range
                         betas_range[:, 1] = beta2_range
-                        betas_range[:, 2] = beta3_range
 
                 for i, beta_delta in enumerate(betas_range):
                     cdata_betas = np.array(cdata['betas']).astype(np.float32)
@@ -296,27 +292,23 @@ def downsample_amass2pytroch(datasets, amass_dir, out_posepath, logger=None, bet
                         assert betas_range % 2 == 0, ValueError('betas_range should be multiple to 2')
                         # if `betas_range` is an integer, 
                         # sample the number of betas1 and betas2 
-                        # that varience from -2. to 2. and betas3 in -5. to 5. as follows:
-                        beta1, beta2, beta3 = cdata['betas'][0], cdata['betas'][1], cdata['betas'][3]
+                        # that varience from -2. to 2. as follows:
+                        beta1, beta2 = cdata['betas'][0], cdata['betas'][1]
                         # left range, right range
                         beta1_lr, beta1_rr = max(0., 2. + beta1), max(0., 2. - beta1)
                         beta2_lr, beta2_rr = max(0., 2. + beta2), max(0., 2. - beta2)
-                        beta3_lr, beta3_rr = max(0., 5. + beta3), max(0., 5. - beta3)
                         # left range percentage, right range percentage
                         beta1_lp, beta1_rp = beta1_lr / (beta1_lr + beta1_rr),  beta1_rr / (beta1_lr + beta1_rr)
                         beta2_lp, beta2_rp = beta2_lr / (beta2_lr + beta2_rr),  beta2_rr / (beta2_lr + beta2_rr)
-                        beta3_lp, beta3_rp = beta3_lr / (beta3_lr + beta3_rr),  beta3_rr / (beta3_lr + beta3_rr)
                         # left range sample number
-                        beta1_ln, beta2_ln, beta3_ln = int(betas_range * beta1_lp), int(betas_range * beta2_lp), int(betas_range * beta3_lp)
+                        beta1_ln, beta2_ln = int(betas_range * beta1_lp), int(betas_range * beta2_lp)
                         # do sampling for beta1 range
                         beta1_range = betas_range_sample(betas_range, beta1, beta1_ln, 2.)
                         beta2_range = betas_range_sample(betas_range, beta2, beta2_ln, 2.)
-                        beta3_range = betas_range_sample(betas_range, beta3, beta3_ln, 5.)
                         # reconstruct beatas_range as numpy.ndarray
                         betas_range = np.zeros((betas_range, len(cdata['betas'])))
                         betas_range[:, 0] = beta1_range
                         betas_range[:, 1] = beta2_range
-                        betas_range[:, 2] = beta3_range
 
                 for i, beta_delta in enumerate(betas_range):
                     cdata_betas = np.array(cdata['betas']).astype(np.float32)
